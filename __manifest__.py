688a8a8670f363052e2388fbe97c8bb42f25566a
GandG-OLN <olivier.nadieline@gandgcorp.com>
2023-03-22 13:23:57 +0000

    [INIT] manifest.py et init.py

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': "BTP",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "NADMAN",
+    'category': 'Technique',
+    'description': """
+    Description text
+    """,
+    # data files always loaded at installation
+    'data': 
+    [
+        #'views/mymodule_view.xml',
+    ],
+    # data files containing optionally loaded demonstration data
+    'demo': 
+    [
+        'demo/demo_data.xml',
+    ],
+}
